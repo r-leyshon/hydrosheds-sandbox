671a4ef581d72ff4e4ae87e8ae00d358d403a2cb
r-leyshon <richard.leyshon@ons.gov.uk>
2024-01-05 07:43:11 +0000

    refactor: extent_from_lads into utility

--- src/hydrosheds/get_basins.py
+++ src/hydrosheds/get_basins.py
@@ -5,33 +5,11 @@ Destination folder is data/hydrosheds-eu/welsh-basins/."""
 import subprocess
 import os
 from tempfile import TemporaryDirectory
-from pathlib import Path
 
 import geopandas as gpd
-import pandas as pd
 from pyprojroot import here
 
-def extent_from_lads(
-    lad_pth: Path=here("data/boundaries/Wales-LAD23.pkl")
-    ) -> gpd.GeoDataFrame:
-    """Read in LAD shapefile & unary union to get extent.
-
-    Parameters
-    ----------
-    lad_pth : Path, optional
-        Path to the LAD shapefile, by default
-        `here("data/boundaries/Wales-LAD23.pkl")`
-
-    Returns
-    -------
-    gpd.GeoDataFrame
-        Full extent of the LAD shapefiles.
-    """
-    boundaries = pd.read_pickle(lad_pth)
-    boundaries = gpd.GeoDataFrame(boundaries, crs=4326)
-    boundary = gpd.GeoDataFrame(
-        {'id':[1],'geometry':[boundaries.unary_union]}, crs=4326)
-    return boundary
+from utilities import extent_from_lads
 
 
 # boundary shapes saved to pickle in src/boundaries/download-ons.property
