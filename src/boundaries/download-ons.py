1d0efe08fc074ae11750e3e6b5a8b7fb84b195f0
r-leyshon <richard.leyshon@ons.gov.uk>
2024-01-14 16:50:53 +0000

    feat: Download UK country shapefiles

--- src/boundaries/download-ons.py
+++ src/boundaries/download-ons.py
@@ -6,10 +6,11 @@ from pyprojroot import here
 import requests
 import geopandas as gpd
 
-ENDPOINT = "https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Local_Authority_Districts_May_2023_UK_BGC_V2/FeatureServer/0/query"
+# ENDPOINT = "https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Local_Authority_Districts_May_2023_UK_BGC_V2/FeatureServer/0/query"
+ENDPOINT = "https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Countries_December_2022_UK_BGC/FeatureServer/0/query?outFields=*&where=1%3D1"
 
 PARAMS = {
-    "where": "LAD23CD like 'W%'", # get only welsh LADs
+    # "where": "LAD23CD like 'W%'", # get only welsh LADs
     "f": "geoJSON",
     "outSR": 4326,
     "outFields": "*" # don't get LAD23NM if you don't ask for this.
@@ -28,6 +29,8 @@ gdf = gpd.GeoDataFrame.from_features(
             # safest to get crs from response
             )
 # drop unneeded columns
-gdf = gdf[["geometry", "LAD23CD", "LAD23NM", "LAD23NMW"]]
+# gdf = gdf[["geometry", "LAD23CD", "LAD23NM", "LAD23NMW"]]
+gdf = gdf[["geometry", "CTRY22CD", "CTRY22NM", "CTRY22NMW"]]
 # write to pickle
-gdf.to_pickle(here("data/boundaries/Wales-LAD23.pkl"))
+# gdf.to_pickle(here("data/boundaries/Wales-LAD23.pkl"))
+gdf.to_pickle(here("data/boundaries/uk-countries.pkl"))
